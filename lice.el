e8558ed218b4d16f6dac1a31037a206cb151502b
Taiki Sugawara <buzz.taiki@gmail.com>
2012-03-18 08:18:02 +0900

    Write some document.

--- lice.el
+++ lice.el
@@ -20,9 +20,28 @@
 
 ;;; Commentary:
 
-;; 
+;; Overview
+;; --------
 
-;;; Code:
+;; `lice.el` provides following features:
+
+;; - License template management.
+;; - File header insertion.
+
+;; Usage
+;; -----
+
+;; Usage is very easy, put `lice.el` in your emacs system, and open a new
+;; file, and run:
+
+;;     M-x lice
+
+;; Then, `lice.el` tell to use which license (default is gpl-3.0). You
+;; can select license on minibuffer completion.
+
+;; When you select license, and enter the `RET`, license and copyright is
+;; putted into a text.
+
 
 (eval-when-compile
   (require 'cl))
